c865d314109e124c82f8d80d2c118950ad515aa6
Pipo <mnsanfilippo@gmail.com>
2023-03-26 17:12:20 -0300

    source

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,5 @@
 module "random" {
-  source           = "../../"
+  source           = "github.com/flufi-io/terraform-module-template"
   length           = var.length
   context          = module.this.context
   override_special = var.override_special
