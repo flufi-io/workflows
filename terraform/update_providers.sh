cde145dcc1d863d85d238cee8aeaecc0f5187105
Pipo Sanfilippo <mnsanfilippo@gmail.com>
2024-05-25 03:30:38 -0300

    Update provider extraction logic and clarify loop comment in update_providers.sh script

--- terraform/update_providers.sh
+++ terraform/update_providers.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Extract the list of providers
-providers=$(terraform providers | awk -F'[][]' '/\] [0-9]+\.[0-9]+\.[0-9]+$/{split($2, a, "/"); print a[2]"/"a[3]}')
+providers=$(terraform providers | grep -E '\] [0-9]+\.[0-9]+\.[0-9]+$' | awk -F'[][]' '{split($2, a, "/"); print a[2]"/"a[3]}')
 
-# Update each provider
+# Iterate over the list and perform the command
 echo "$providers" | while read -r provider; do
   echo "Updating provider: $provider"
   tfupdate provider -r "$provider" .
